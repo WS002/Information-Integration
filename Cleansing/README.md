ba36509b05c8e5ebf72b4af97ac08bbc47ece587
WS002 <ivanovnikolay25@yahoo.de>
2017-01-09 21:41:12 +0100

    The script now also cleanses and merges the wiki articles, with the rest

--- Cleansing/README.md
+++ Cleansing/README.md
@@ -6,12 +6,21 @@
 1. Perform Sorted Neigborhood Algorithm on the matched_schema. Key for each row is defined as all consonants, taken from the name and category fields.
 2. The result is then being sorted alphabetically.
 3. A defined window moves through the sorted list to find duplicates.
-4. Calculate the euclidean distance between the coordinates of each pair inside the moving window and define a certain threshold. If the distance is less than the threshold, mark the pair as a duplicate. ( In our case, the threshold are the 0's after the floating point. E.g. for 0.000**** works quite fine )
+4. 
+	4.1 Calculate the euclidean distance between the coordinates of each pair inside the moving window and define a certain threshold. If the distance is less than the threshold, mark the pair for duplicate candidate. ( In our case, the threshold are the 0's after the floating point. E.g. for 0.000**** works quite fine )
+	4.2 Calculate the levenshtein distance between the keys string. If the distance is bigger than the defined threshold, mark the pair for duplicate candidate.
+	4.3 Take both thresholds ONLY if the pair fields exist. For merging reasons, take only these pairs, in which at least one item has coordinates! 
+
+
 5. Merge the duplicates according to these rules:
-	5.1. Calculate the AVG between the latitude and longitude of the pairs
+	5.1. Calculate the AVG between the latitude and longitude of the pairs ( or take either of the coordinates, if one of items in the pair does not contain coordinates )
 	5.2  Take a certain field only if it is existent in at least one of the items in the pairs
 	5.3  The category, starting with a capital letter is preferred
 
 
+This should work for the wiki articles as well, each of which will try to get merged with an item with coordinates, based only on the levenshtein distance of the key, without euclidean distance ( no coordinates )
 
-P.S. The code is adjusted to work only for rows with defined coordinates. The wiki articles can be adjusted accordingly by performing the same algorithm only with name and/or description or using a totally different algorithm. You decide :)
+6. Rerun the script until all data converges, that is, until nothing changes.
+
+
+
--- Cleansing/cleansing.py
+++ Cleansing/cleansing.py
@@ -3,11 +3,14 @@ import sys
 sys.path.append('../')
 import database
 import math
+import Levenshtein
 
 blacklist = ['a', 'e', 'i', 'o', 'u', ' ', '-', ':', '.', ',', ';', '`', '\'', '"', '<', '>', '(', ')', '+', '=', '&', '#', '$', '^', '*', '@', '%', '!', '?', '/', '|' ]
 
 # Duplicate after certain floating point number, e.g. 1000 => after third comma digit 
 floatingPointThreshold = 1000
+
+levenshteinThreshold = 0.9
 
 # Get instance of the database connection
 myDB = database.DBConnection()
@@ -18,14 +21,18 @@ def _createAndSortKeys(data):
 	keyWithCoordinates = []
 	for index, row in enumerate(data):
 		# row[0] = ID, 1 = name, 2 = lat, 3 = lng, 4 = tourismCategory, 5 = checkinsCount, 6 = description
-		if index + 1 < len(data) and row[2] != '' and row[2] is not None and row[3] != '' and row[3] is not None:
+		if index + 1 < len(data):
 			key = ''
-			for c in row[1]:
-				if c not in blacklist: 
-					key += c.upper()
-			for c in row[4]:
-				if c not in blacklist:
-					key += c.upper()
+
+			if row[1] is not None and row[1] != '':
+				for c in row[1]:
+					if c not in blacklist: 
+						key += c.upper()
+
+			if row[4] is not None and row[4] != '':
+				for c in row[4]:
+					if c not in blacklist:
+						key += c.upper()
 
 			keyWithCoordinates.append([key, row])			
 
@@ -38,6 +45,9 @@ def _euclideanDistance(x1, x2):
 	lngDistance = float(x1[1]) - float(x2[1])
 	return math.sqrt(latDistance * latDistance + lngDistance * lngDistance)
 
+def _levenshteinDistance(str1, str2):
+	return Levenshtein.distance(str1, str2)
+
 def _compare(data, windowSize):
 	if windowSize < 2:
 		return 0 
@@ -46,15 +56,33 @@ def _compare(data, windowSize):
 
 	for index, item in enumerate(data):	
 		if index+windowSize < len(data):			
-			for secondItem in data[(index+1):(index+windowSize)]:				
-				x1 = [item[1][2], item[1][3]]
-				x2 = [secondItem[1][2], secondItem[1][3] ]
-				euDistance = _euclideanDistance(x1, x2)
-				if int(euDistance * 1000) == 0:
-					duplicateCandidates.append([item, secondItem])
+			for secondItem in data[(index+1):(index+windowSize)]:	
+				euDistance = -1
+				levenshteinDistance = -1
+				if item[1][2] is not None and item[1][3] is not None and secondItem[1][2] is not None and secondItem[1][3] is not None:		
+					x1 = [item[1][2], item[1][3]]
+					x2 = [secondItem[1][2], secondItem[1][3] ]
+					euDistance = _euclideanDistance(x1, x2)
 
+				# At least one of the items in the pair must have coordinates
+				if ( (item[1][2] is not None and item[1][3] is not None) or (secondItem[1][2] is not None and secondItem[1][3] is not None) ):			
+					
+
+					if item[0] is not None and item[0] != '' and secondItem[0] is not None and secondItem[0] != '':					
+						levenshteinDistance = _levenshteinDistance(item[0], secondItem[0])
 				
+					if euDistance > -1 and levenshteinDistance > -1:
+						if int(euDistance * floatingPointThreshold) == 0 and levenshteinDistance >= levenshteinThreshold:
+							duplicateCandidates.append([item, secondItem])
+					elif euDistance > -1:
+						if int(euDistance * floatingPointThreshold) == 0:
+							duplicateCandidates.append([item, secondItem])
+					elif levenshteinDistance > -1 and levenshteinDistance >= levenshteinThreshold:
+							duplicateCandidates.append([item, secondItem])
+
 	return duplicateCandidates
+
+
 
 
 def _merge(duplicates):
@@ -63,16 +91,18 @@ def _merge(duplicates):
 		firstItem =  item[0][1]
 	        secondItem =  item[1][1]
 
-		values = ''
+		valueString = ''
+		values = []
 		keys = ''
-
+		
 		# take one of the names
 		if firstItem[1] != '' and firstItem[1] is not None:
-			values += '"' + firstItem[1] + '"'
+			valueString += '%s'
+			values.append(firstItem[1])
 			keys += 'name'
-			print values
 		elif secondItem[1] != '' and secondItem[1] is not None:
-			values += '"' + secondItem[1] + '"'
+			valueString += '%s'
+			values.append(firstItem[1])
 			keys += 'name'
 
 			
@@ -81,47 +111,66 @@ def _merge(duplicates):
 		if firstItem[2] is not None and firstItem[2] != '' and secondItem[2] is not None and secondItem[2] != '':
 			lat = (float(firstItem[2]) + float(secondItem[2])) / 2.0
 			lng = (float(firstItem[3]) + float(secondItem[3])) / 2.0
-			values += ', "' + str(lat) + '", "' + str(lng) + '"'
-			keys += ', lat, lng'			
+			valueString += ',%s, %s'
+			values.append(str(lat))
+			values.append(str(lng))
+			keys += ', lat, lng'	
+		elif firstItem[2] is not None and firstItem[2] != '':
+			lat = firstItem[2]
+			lng = firstItem[3]
+			valueString += ',%s, %s'
+			values.append(str(lat))
+			values.append(str(lng))
+			keys += ', lat, lng'
+		else:
+			lat = secondItem[2]
+			lng = secondItem[3]
+			valueString += ',%s, %s'
+			values.append(str(lat))
+			values.append(str(lng))
+			keys += ', lat, lng'		
 
 
 		# take the category that starts with capital letter
 		if firstItem[4] is not None and firstItem[4] != '' and firstItem[4][0].isupper():
-			values += ', "' + firstItem[4] + '"'
+			valueString += ', %s'
+			values.append(firstItem[4])
 			keys += ', tourismCategory'
 		elif secondItem[4] is not None and secondItem[4] != '':
-			values += ', "' + secondItem[4] + '"'
+			valueString += ', %s'
+			values.append(secondItem[4])
 			keys += ', tourismCategory'
 
 		# take the checkinsCount != None 
 		if firstItem[5] != '' and firstItem[5] is not None:
-			values += ', "' + str(firstItem[5]) + '"'
+			values.append(str(firstItem[5]))
+			valueString += ', %s'
 			keys += ', checkinsCount'
 		elif secondItem[5] != '' and secondItem[5] is not None:
-			values += ', "' + str(secondItem[5]) + '"'
+			values.append(str(secondItem[5]))
+			valueString += ', %s'
 			keys += ', checkinsCount'
 		
 		# same for description
 		if firstItem[6] != '' and firstItem[6] is not None:
-			values += ', "' + firstItem[6] + '"'
+			values.append(firstItem[6])
+			valueString += ', %s'
 			keys += ', description'
 		elif secondItem[6] != '' and secondItem[6] is not None:
-			values += ', "' + secondItem[6] + '"'
+			values.append(secondItem[6])
+			valueString += ', %s'
 			keys += ', description'
+
+		print "Executing ... "
+		print "INSERT INTO matched_schema ("+keys+") VALUES ("+''.join(values)+")" 
+		sql="INSERT INTO matched_schema ("+keys+") VALUES ("+valueString+")"
+		myDB.executeQuery(sql, values)		
 
 		print "Executing ... " 
 		print "DELETE FROM matched_schema WHERE ID = " + str(firstItem[0]) + " OR ID = " + str(secondItem[0]) 
 		sql="DELETE FROM matched_schema WHERE ID = " + str(firstItem[0]) + " OR ID = " + str(secondItem[0])
-		cursor = myDB.conn.cursor()
-		cursor.execute(sql)
-		myDB.conn.commit()
+		myDB.executeQuery(sql, [])	
 
-		print "Executing ... "
-		print "INSERT INTO matched_schema ("+keys+") VALUES ("+values+")" 
-		sql="INSERT INTO matched_schema ("+keys+") VALUES ("+values+")"
-		cursor = myDB.conn.cursor()
-		cursor.execute(sql)
-		myDB.conn.commit()
 
 def sortedNeighbourhood(data):
 	# Duplicate detection in 3 steps:
